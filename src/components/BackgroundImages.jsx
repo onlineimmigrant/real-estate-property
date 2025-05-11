19c11d2a8849510aeed77e2ac43f6d376b51f248
Andrei Vialichka <ois@OIS-max-macbook-pro.local>
2025-05-11 17:43:40 +0100

    media new

--- src/components/BackgroundImages.jsx
+++ src/components/BackgroundImages.jsx
@@ -15,7 +15,7 @@ function BackgroundImages() {
       <img
         src={HeroImageMobile}
         className="absolute w-3/4 h-3/4 mx-auto my-auto object-contain opacity-70 animate-moveHorizontal sm:hidden"
-        alt="Премиум коммерческое помещение на Петра Мстиславца 1, Минск (мобильная версия)"
+        alt="Премиум коммерческое помещение на Петра Мстиславца 1, Минск"
         loading="lazy"
         style={{ zIndex: 1 }}
       />
